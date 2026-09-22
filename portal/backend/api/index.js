5c297cb67f8d6e87fc3583c8e129355a2fa5134c
gillion <waleedjutt3312@gmail.com>
2026-09-22 15:28:14 +0500

    Show database connection error

--- portal/backend/api/index.js
+++ portal/backend/api/index.js
@@ -12,12 +12,13 @@ export default async function handler(req, res) {
     await dbPromise;
 
     return app(req, res);
-  } catch (error) {
+  }    catch (error) {
     console.error('MongoDB connection failed:', error);
 
     return res.status(500).json({
       success: false,
       message: 'Database connection failed',
+      error: error.message,
     });
   }
 }
