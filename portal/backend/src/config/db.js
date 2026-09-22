e0941b5bf024fafb09a8f8b0a4fbfc397bffe9ad
gillion <waleedjutt3312@gmail.com>
2026-09-22 17:46:56 +0500

    Disable MongoDB buffering and verify connection

--- portal/backend/src/config/db.js
+++ portal/backend/src/config/db.js
@@ -3,9 +3,29 @@ import env from './env.js';
 import logger from '../utils/logger.js';
 
 mongoose.set('strictQuery', true);
+mongoose.set('bufferCommands', false);
+
 export const connectDB = async (uri = env.mongoUri) => {
-  const connection = await mongoose.connect(uri, { autoIndex: !env.isProduction });
-  logger.info(`MongoDB connected: ${connection.connection.host}/${connection.connection.name}`);
+  if (!uri) {
+    throw new Error('MONGO_URI is missing');
+  }
+
+  const connection = await mongoose.connect(uri, {
+    autoIndex: !env.isProduction,
+    serverSelectionTimeoutMS: 10000,
+    connectTimeoutMS: 10000,
+  });
+
+  if (connection.connection.readyState !== 1) {
+    throw new Error(
+      `MongoDB connection is not ready. readyState=${connection.connection.readyState}`
+    );
+  }
+
+  logger.info(
+    `MongoDB connected: ${connection.connection.host}/${connection.connection.name}`
+  );
+
   return connection;
 };
 
